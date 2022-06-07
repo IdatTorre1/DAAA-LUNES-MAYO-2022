56574d89e04b69a21fbd09f7fc7d7146a05982a8
alexis5999 <alexis5999@gmail.com>
2022-06-06 21:09:34 -0500

    primer commit

---
+++ Semana01-STS/src/main/java/com/idat/edu/pe/Semana01StsApplication.java
@@ -0,0 +1,13 @@
+package com.idat.edu.pe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Semana01StsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Semana01StsApplication.class, args);
+	}
+
+}
